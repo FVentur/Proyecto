c1a01956178d1317c9e5a195876278d4ad9fae76
FVentur <80140932+FVentur@users.noreply.github.com>
2021-09-14 20:24:36 -0300

    Add files via upload

---
+++ Sonido.py
@@ -0,0 +1,83 @@
+from time import sleep
+from tkinter import *
+import random
+import winsound
+
+lista = []
+otralista = []
+pc = [1100, 1400, 1800, 1600]
+def unsort(aux):
+    pcdesordenada=[]
+    for x in range(len(aux)):
+        azar=random.choice(aux)
+        pcdesordenada.append(azar)
+    return pcdesordenada
+def botn1():
+    rango=1100
+    aux= True
+    sonido(rango, aux)
+
+def botn2():
+    rango=1400
+    aux= True
+    sonido(rango, aux)
+
+def botn3():
+    rango=1600
+    aux= True
+    sonido(rango, aux)
+
+def botn4():
+    rango=1800
+    aux= True
+    sonido(rango, aux)
+
+def sonido(aux, boolean):
+    global pcdesordenada, lista, pc
+    winsound.Beep(aux,300)
+    if boolean:
+        lista.append(aux)
+        comparacion(pcdesordenada, lista, pc)
+def Ejecutar(lis):
+    aux = False
+    for numero in (lis):
+        sonido(numero, aux)
+
+def comparacion(lisdes, li, pc):
+    cont = 0
+    for item,item2 in zip(lisdes, li):
+        if item.__eq__(item2):
+            cont += 1
+        else:
+            li.clear()
+            puntaje.set(0)
+            for i in range (len(lisdes)):
+                puntaje.set(0)
+                if i.__gt__(3):
+                    lisdes.pop(4)
+            break
+    if cont.__ge__(len(lisdes)):
+        puntaje.set(int(puntaje.get())+1)
+        aux = random.choice(pc)
+        lisdes.append(aux)
+        li.clear()
+
+pcdesordenada = unsort(pc)
+
+
+Ventana=Tk()
+Ventana.geometry("700x500")
+Ventana.config(bg='black')
+
+puntaje = IntVar()
+label = Label(Ventana, textvariable=puntaje).grid(row=0, column=2)
+
+
+boton = Button(Ventana, text="iniciar sonido", command=botn1, height = 15, width = 35, bg='red').grid(row=1, column=1)
+boton2 = Button(Ventana, text="iniciar sonido", command=botn2, height = 15, width = 35, bg='green').grid(row=1, column=2)
+boton3 = Button(Ventana, text="iniciar sonido", command=botn3, height = 15, width = 35, bg='yellow').grid(row=2, column=1)
+boton4 = Button(Ventana, text="iniciar sonido", command=botn4, height = 15, width = 35, bg='blue').grid(row=2, column=2)
+
+boton5 = Button(Ventana, text="ejecutar sonido", command=lambda: Ejecutar(pcdesordenada), bg='orange').grid(row=0, column=0)
+Ventana.mainloop()
+
