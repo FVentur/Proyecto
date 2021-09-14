ea638d71094bc31ff7744878b83d3a8cee2e71b6
Ventura <vfran9306@gmail.com>
2021-09-14 18:42:57 -0300

    version 2

--- Sonido.py
+++ Sonido.py
@@ -1,13 +1,35 @@
 from time import sleep
 from pyo import *
 from tkinter import *
-def sonido():
+from random import randint
+
+lista=[]
+
+def random():
+    rango=randint(100,1000)
+    aux = True
+    sonido(rango, aux)
+
+def sonido(aux, boolean):
+    global lista
     server = Server().boot()
     server.start()
-    sine = Sine(300.63, mul=0.1).out()
-    sleep(3)
+    sine = Sine(aux, mul=4.2).out()
+    sleep(1)
+
+    if boolean:
+
+        lista.append(aux)
+        print(lista)
     server.stop()
+def Ejecutar(lis):
+    aux = False
+    for numero in (lis):
+        sonido(numero, aux)
+
+
 Ventana=Tk()
 Ventana.geometry("300x300")
-boton = Button(Ventana, text="ejecutar sonido", command=sonido).grid()
+boton = Button(Ventana, text="iniciar sonido", command=random, height = 20, width = 20, bg='red').grid(row=0, column=0)
+boton2 = Button(Ventana, text="ejecutar sonido", command=lambda: Ejecutar(lista), height = 20, width = 20, bg='green').grid(row=0, column=1)
 Ventana.mainloop()
