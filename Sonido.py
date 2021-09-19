2623cf21ffcf60c0200eb4ef9879e48a017d9dea
FVentur <80140932+FVentur@users.noreply.github.com>
2021-09-19 13:53:03 -0300

    Add files via upload

--- Sonido.py
+++ Sonido.py
@@ -41,8 +41,7 @@ def sonido(aux, boolean):
 def Ejecutar(lis):
     aux = False
     for numero in (lis):
-        sonido(numero, aux)
-
+            sonido(numero, aux)
 def comparacion(lisdes, li, pc):
     cont = 0
     for item,item2 in zip(lisdes, li):
@@ -52,32 +51,53 @@ def comparacion(lisdes, li, pc):
             li.clear()
             puntaje.set(0)
             for i in range (len(lisdes)):
-                puntaje.set(0)
-                if i.__gt__(3):
-                    lisdes.pop(4)
+                cont = 0
+                if i.__gt__(0):
+                    lisdes.pop(1)
             break
     if cont.__ge__(len(lisdes)):
         puntaje.set(int(puntaje.get())+1)
         aux = random.choice(pc)
         lisdes.append(aux)
         li.clear()
+def sonido_teclado(event):
+    if event.keysym.__eq__('Up'):
+        botn4()
+    if event.keysym.__eq__('Down'):
+        botn1()
+    if event.keysym.__eq__('Left'):
+        botn3()
+    if event.keysym.__eq__('Right'):
+        botn2()
+    if event.keysym.__eq__('space'):
+        Ejecutar(pcdesordenada)
+
 
 pcdesordenada = unsort(pc)
 
 
 Ventana=Tk()
-Ventana.geometry("700x500")
+Ventana.geometry("700x520")
 Ventana.config(bg='black')
-
+canvas = Canvas(Ventana,height = 15, width = 35, bg='black')
+canvas.grid()
 puntaje = IntVar()
-label = Label(Ventana, textvariable=puntaje).grid(row=0, column=2)
+label = Label(Ventana, textvariable=puntaje, font=("Comic Sans", 20), bg='black', fg="white").grid(row=0, column=2)
 
 
-boton = Button(Ventana, text="iniciar sonido", command=botn1, height = 15, width = 35, bg='red').grid(row=1, column=1)
-boton2 = Button(Ventana, text="iniciar sonido", command=botn2, height = 15, width = 35, bg='green').grid(row=1, column=2)
-boton3 = Button(Ventana, text="iniciar sonido", command=botn3, height = 15, width = 35, bg='yellow').grid(row=2, column=1)
-boton4 = Button(Ventana, text="iniciar sonido", command=botn4, height = 15, width = 35, bg='blue').grid(row=2, column=2)
+canvas.bind_all('<KeyPress-Up>',sonido_teclado)
+canvas.bind_all('<KeyPress-Down>',sonido_teclado)
+canvas.bind_all('<KeyPress-Left>',sonido_teclado)
+canvas.bind_all('<KeyPress-Right>',sonido_teclado)
+canvas.bind_all('<space>',sonido_teclado)
+
+boton = Button(Ventana, command=botn1, height = 15, width = 35, bg='red').grid(row=1, column=1)
+boton2 = Button(Ventana, command=botn2, height = 15, width = 35, bg='green').grid(row=1, column=2)
+boton3 = Button(Ventana, command=botn3, height = 15, width = 35, bg='yellow').grid(row=2, column=1)
+boton4 = Button(Ventana, command=botn4, height = 15, width = 35, bg='blue').grid(row=2, column=2)
 
 boton5 = Button(Ventana, text="ejecutar sonido", command=lambda: Ejecutar(pcdesordenada), bg='orange').grid(row=0, column=0)
+
+
 Ventana.mainloop()
 
